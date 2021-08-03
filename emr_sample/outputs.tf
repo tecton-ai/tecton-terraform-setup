c1be230600b430560a27f2c60d5d797ff89e1c89
Daryl Neubieser <daryl@tecton.ai>
2021-08-03 13:25:58 -0700

    Fix template's outputs

--- emr_sample/outputs.tf
+++ emr_sample/outputs.tf
@@ -5,10 +5,10 @@ output "region" {
   value = local.region
 }
 output "cross_account_role_arn" {
-  value = module.tecton.aws_iam_role.cross_account_role.arn
+  value = module.tecton.cross_account_role_arn
 }
 output "cross_account_external_id" {
-  value = resource.random_id.external_id.id
+  value = random_id.external_id.id
 }
 output "spark_role_name" {
   value = local.spark_role_name
