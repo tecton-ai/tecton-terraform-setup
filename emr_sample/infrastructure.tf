9a4b520c94c1c10c8de999dc1fbdee2a649b46aa
Rohit Agrawal <rohit@tecton.ai>
2022-01-10 10:04:17 -0800

    VPC V2 EMR

--- emr_sample/infrastructure.tf
+++ emr_sample/infrastructure.tf
@@ -8,51 +8,111 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "this-accounts-region"
+# Fill these in
+variable "deployment_name" {
+  type = string
 }
 
-locals {
-  deployment_name = "my-deployment-name"
+variable "region" {
+  type = string
+}
 
-  # The region and account_id of your production AWS account
-  region     = "my-region"
-  account_id = "1234567890"
+variable "account_id" {
+  type = string
+}
 
-  # Get this values from your Tecton rep
-  tecton_assuming_account_id = "1234567890"
+variable "spark_role_name" {
+  type = string
+}
 
-  # OPTIONAL for EMR notebook clusters in a different account (see optional block at end of file)
-  # cross_account_arn = "arn:aws:iam::9876543210:root"
+variable "tecton_dataplane_account_role_arn" {
+  type = string
+}
+
+variable "is_vpc_deployment" {
+  type = bool
+  default = false
+}
+
+variable "elasticache_enabled" {
+  type = bool
+  default = false
+}
+
+variable "ip_whitelist" {
+  default = []
+}
+
+variable "tecton_assuming_account_id" {
+  type = string
+  description = "Get this from your Tecton rep"
+}
+
+provider "aws" {
+  region = "this-accounts-region"
 }
 
 resource "random_id" "external_id" {
   byte_length = 16
 }
 
-module "tecton" {
-  source                     = "../deployment"
-  deployment_name            = local.deployment_name
-  account_id                 = local.account_id
-  tecton_assuming_account_id = local.tecton_assuming_account_id
-  region                     = local.region
-  cross_account_external_id  = random_id.external_id.id
-
-  create_emr_roles = true
+module "subnets" {
+  source          = "../emr/vpc_subnets"
+  deployment_name = var.deployment_name
+  region          = var.region
 }
 
 module "security_groups" {
   source          = "../emr/security_groups"
-  deployment_name = local.deployment_name
-  region          = local.region
+  deployment_name = var.deployment_name
+  region          = var.region
   emr_vpc_id      = module.subnets.vpc_id
 }
 
 # optionally, use a Tecton default vpc/subnet configuration
-module "subnets" {
-  source          = "../emr/vpc_subnets"
-  deployment_name = local.deployment_name
-  region          = local.region
+module "eks_subnets" {
+  providers = {
+    aws = aws
+  }
+  count           = var.is_vpc_deployment ? 1 : 0
+  source          = "../eks/vpc_subnets"
+  deployment_name = var.deployment_name
+  region          = var.region
+}
+
+module "eks_security_groups" {
+  providers = {
+    aws = aws
+  }
+  count             = var.is_vpc_deployment ? 1 : 0
+  source            = "../eks/security_groups"
+  deployment_name   = var.deployment_name
+  cluster_vpc_id    = module.eks_subnets[0].vpc_id
+  ip_whitelist      = concat([for ip in module.eks_subnets[0].eks_subnet_ips: "${ip}/32"], var.ip_whitelist)
+  tags              = {"tecton-accessible:${var.deployment_name}": "true"}
+}
+
+module "tecton" {
+  source                     = "../deployment"
+  deployment_name            = var.deployment_name
+  account_id                 = var.account_id
+  tecton_assuming_account_id = var.tecton_assuming_account_id
+  region                     = var.region
+  cross_account_external_id  = random_id.external_id.id
+  create_emr_roles           = true
+  elasticache_enabled        = var.elasticache_enabled
+}
+
+module "tecton_vpc" {
+  count                      = var.is_vpc_deployment ? 1 : 0
+  source                     = "../vpc_deployment"
+  deployment_name            = var.deployment_name
+  account_id                 = var.account_id
+  tecton_assuming_account_id = var.tecton_assuming_account_id
+  region                     = var.region
+  cross_account_external_id  = random_id.external_id.id
+  create_emr_roles           = true
+  elasticache_enabled        = var.elasticache_enabled
 }
 
 module "notebook_cluster" {
@@ -63,8 +123,8 @@ module "notebook_cluster" {
   # Set count = 1 once your Tecton rep confirms Tecton has been deployed in your account
   count = 0
 
-  region          = local.region
-  deployment_name = local.deployment_name
+  region          = var.region
+  deployment_name = var.deployment_name
   instance_type   = "m5.xlarge"
 
   subnet_id            = module.subnets.emr_subnet_id
@@ -85,7 +145,7 @@ module "notebook_cluster" {
   # ]
 
   has_glue        = true
-  glue_account_id = local.account_id
+  glue_account_id = var.account_id
 }
 
 # This module adds some IAM privileges to enable your Tecton technical support
@@ -97,7 +157,7 @@ module "emr_debugging" {
   source = "../emr/debugging"
 
   count                   = 0
-  deployment_name         = local.deployment_name
+  deployment_name         = var.deployment_name
   cross_account_role_name = module.tecton.cross_account_role_name
 }
 
@@ -110,6 +170,11 @@ module "emr_debugging" {
 # To use EMR notebooks in a different account than your Tecton account, uncomment the below
 # modules and also the relevant local vars
 ##############################################################################################
+
+# locals {
+#   OPTIONAL for EMR notebook clusters in a different account (see optional block at end of file)
+#   cross_account_arn = "arn:aws:iam::9876543210:root"
+# }
 
 # provider "aws" {
 #   region = "this-accounts-region"
@@ -131,14 +196,14 @@ module "emr_debugging" {
 #   source = "../emr/cross_account"
 
 #   cidr_block              = "10.0.0.0/16"
-#   deployment_name         = local.deployment_name
+#   deployment_name         = var.deployment_name
 #   enable_notebook_cluster = true
-#   region                  = local.region
+#   region                  = var.region
 #   # roles below created by `aws emr create-default-roles`
 #   # note that this role also needs access to S3 and Secretsmanager
 #   emr_instance_profile_name = "EMR_EC2_DefaultRole"
 #   emr_service_role_name     = "EMR_DefaultRole"
-#   glue_account_id           = local.account_id
+#   glue_account_id           = var.account_id
 # }
 
 # # gives the cross-account permissions to read the materialized data bucket
@@ -151,7 +216,7 @@ module "emr_debugging" {
 #         Sid    = "AllowReadOnly"
 #         Effect = "Allow"
 #         Principal = {
-#           "AWS" : local.cross_account_arn
+#           "AWS" : var.cross_account_arn
 #         }
 #         Action = ["s3:Get*", "s3:List*"]
 #         Resource = [
