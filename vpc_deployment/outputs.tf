1226933f35a1fea6bf4d022c2e3d9707b2f015bf
amargvela <akaki@tecton.ai>
2022-02-16 18:23:55 -0800

    Fix the role for the debugging perms

--- vpc_deployment/outputs.tf
+++ vpc_deployment/outputs.tf
@@ -1,13 +1,19 @@
 output "spark_role_name" {
   value = var.spark_role_name
 }
+
 output "spark_role_arn" {
   value = var.create_emr_roles ? aws_iam_role.emr_spark_role[0].name : null
 }
+
 output "emr_master_role_name" {
   value = var.create_emr_roles ? aws_iam_role.emr_master_role[0].name : null
 }
+
 output "emr_spark_instance_profile_arn" {
   value = var.create_emr_roles ? aws_iam_instance_profile.emr_spark_instance_profile[0].arn : null
 }
 
+output "devops_role_name" {
+  value = aws_iam_role.devops_role.name
+}
