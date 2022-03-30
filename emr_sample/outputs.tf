bcc2232da9ad43f73ec1fea5f20b7aafb07cb69f
Scott Ames <8071752+scottames@users.noreply.github.com>
2022-03-30 08:44:45 -0700

    [feat][vpc_1] Resources to support EKS VPC endpoint (in own VPC) (#37)
    ## What
    
    **_Note:_** This pull request is into the [`vpc_1`](https://github.com/tecton-ai-ext/tecton-terraform-setup/tree/vpc_1) branch, which is tracking off of the [`vpc_1.0`](https://github.com/tecton-ai-ext/tecton-terraform-setup/releases/tag/vpc_1.0) tag, _not_ the latest from the [`vpc`](https://github.com/tecton-ai-ext/tecton-terraform-setup/tree/vpc) branch. These changes as applied to the latest `vpc` branch can be found in https://github.com/tecton-ai-ext/tecton-terraform-setup/pull/40.
    
    Added resources (security group + IAM policy) which enable the addition of a VPC endpoint within the EKS VPC.
    
    ## Why
    
    These resources enable the creation of a VPC endpoint within the EKS VPC, which is used for routing traffic to the Tecton cluster appropriately.
    
    ## Notes
    
    This change includes some formatting fixes. Because Terraform does not care about whitespace, I recommend toggling the Github diff to [ignore whitespace changes](https://github.blog/2018-05-01-ignore-white-space-in-code-review/).

--- emr_sample/outputs.tf
+++ emr_sample/outputs.tf
@@ -7,29 +7,29 @@ output "region" {
 }
 
 output "cross_account_role_arn" {
-  value = var.is_vpc_deployment ? null : module.tecton.cross_account_role_arn
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? null : module.tecton.cross_account_role_arn) : null
 }
 
 output "spark_role_arn" {
-  value = var.is_vpc_deployment ? module.tecton_vpc[0].spark_role_arn : module.tecton.spark_role_arn
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? module.tecton_vpc[0].spark_role_arn : module.tecton.spark_role_arn) : null
 }
 
 output "spark_instance_profile_arn" {
-  value = var.is_vpc_deployment ? module.tecton_vpc[0].emr_spark_instance_profile_arn : module.tecton.emr_spark_instance_profile_arn
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? module.tecton_vpc[0].emr_spark_instance_profile_arn : module.tecton.emr_spark_instance_profile_arn) : null
 }
 
 output "vpc_id" {
-  value = var.is_vpc_deployment ? module.eks_subnets[0].vpc_id : ""
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? module.eks_subnets[0].vpc_id : "") : null
 }
 
 output "eks_subnet_ids" {
-  value = var.is_vpc_deployment ? module.eks_subnets[0].eks_subnet_ids : []
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? module.eks_subnets[0].eks_subnet_ids : []) : null
 }
 
 output "public_subnet_ids" {
-  value = var.is_vpc_deployment ? module.eks_subnets[0].public_subnet_ids : []
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? module.eks_subnets[0].public_subnet_ids : []) : null
 }
 
 output "security_group_ids" {
-  value = var.is_vpc_deployment ? [module.eks_security_groups[0].eks_security_group_id, module.eks_security_groups[0].eks_worker_security_group_id, module.eks_security_groups[0].rds_security_group_id] : []
+  value = var.apply_layer > 1 ? (var.is_vpc_deployment ? [module.eks_security_groups[0].eks_security_group_id, module.eks_security_groups[0].eks_worker_security_group_id, module.eks_security_groups[0].rds_security_group_id] : []) : null
 }
