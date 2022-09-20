e44e15bd35b3b8f73edb01878537e5e00bc10492
Scott Ames <scott@tecton.ai>
2022-09-20 07:32:04 -0700

    Revert "feat: add cluster s3 bucket (#52)" (#54)
    # What
    
    Revert #52 
    
    # Why
    
    This causes resource conflicts and should be added by Tecton deployment automation after this Terraform is applied.

--- s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "deployment_name" {
-  type = string
-}
-
-variable "additional_s3_read_only_principals" {
-  type        = list(string)
-  default     = []
-  description = "Additional principals that should be able to access (read-only) the cluster s3 bucket."
-}
