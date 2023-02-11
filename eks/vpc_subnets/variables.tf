4afa032bd445d10a13ed380c504d54f5b11268c4
yanhuang <yan@tecton.ai>
2023-02-10 18:18:32 -0800

    one more update

--- eks/vpc_subnets/variables.tf
+++ eks/vpc_subnets/variables.tf
@@ -7,20 +7,9 @@ variable "availability_zone_count" {
   description = "The number of availability zones for Tecton to use EKS in. Please set this to 3 unless the region you are deploying to only has 2 AZs."
 }
 
-variable "satellite_availability_zone_count" {
-  type        = number
-  description = "The number of availability zones for Tecton to use EKS in. Please set this to 3 unless the region you are deploying to only has 2 AZs."
-}
-
 variable "region" {
   type        = string
   description = "The region for Tecton to use EKS in."
-}
-
-variable "satellite_region" {
-  type        = string
-  description = "The satellite region for Tecton to use EKS in."
-  default     = ""
 }
 
 variable "eks_vpc_id" {
@@ -29,26 +18,11 @@ variable "eks_vpc_id" {
   description = "Id of a pre-existing VPC."
 }
 
-variable "eks_satellite_vpc_id" {
-  type        = string
-  default     = null
-  description = "Id of a pre-existing satellite VPC."  
-}
-
 variable "eks_subnet_cidr_prefix" {
   type        = string
   description = "The CIDR block for the private and public subnets for this module to create."
   validation {
     condition     = tonumber(regex("/([0-9]+)", var.eks_subnet_cidr_prefix)[0]) <= 18
-    error_message = "Subnet must have enough space: the smallest acceptable prefix is /18."
-  }
-}
-
-variable "eks_satellite_subnet_cidr_prefix" {
-  type        = string
-  description = "The CIDR block for the private and public subnets for this module to create."
-  validation {
-    condition     = tonumber(regex("/([0-9]+)", var.eks_satellite_subnet_cidr_prefix)[0]) <= 18
     error_message = "Subnet must have enough space: the smallest acceptable prefix is /18."
   }
 }
