9bc3c64a6276fc6a73135cb8089311488a4b7b94
yanhuang <yan@tecton.ai>
2023-02-16 11:03:45 -0800

    update

--- roles/roles.tf
+++ roles/roles.tf
@@ -117,7 +117,7 @@ data "template_file" "devops_fargate_role_json" {
     FARGATE_POLICY_ARNS     = jsonencode(
       concat(
         [aws_iam_policy.eks_fargate_node_policy[0].arn],
-        [for region in aws_iam_policy.eks_fargate_satellite_node_policy: aws_iam_policy.eks_fargate_satellite_node_policy[region].arn]
+        [aws_iam_policy.eks_fargate_satellite_node_policy.*.arn]
       )
     )
   }
