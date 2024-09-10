087e327036179c7d7c702db2d86b997c6f335cc7
Zhou Tan <zhou@tecton.ai>
2024-09-10 10:39:24 -0700

    feat: separate flags controlling spark and rift iam resources [DVOPS-1809] (#144)
    Previously, we use a single variable `use_rift_cross_account_policy` to toggle between using Rift and Spark compute cross-account policy. This is no longer sufficient after we introduce rift on data plane. Now, we have to differentiate between:
    - rift on control plane
    - rift on data plane
    - spark
    
    This PR introduces to separate flags to control:
    1. enablement of rift on control plane: `use_rift_compute_on_control_plane`
    2. enablement of spark: `use_spark_compute`
    
    The 4 different combinations of these two flags are:
    - #1 on, #2 on: enable both rift on control plane and spark
    - #1 on, #2 off: enable rift on control plane
    - #1 off, #2 on: enable spark
    - #1 off, #2 off: enable rift on data plane (must also include `rift` module)

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -8,7 +8,7 @@ output "spark_role_name" {
   value = local.spark_role_name
 }
 output "spark_role_arn" {
-  value = var.use_rift_cross_account_policy ? null : data.aws_iam_role.spark_role[0].arn
+  value = local.use_spark_compute ? data.aws_iam_role.spark_role[0].arn : null
 }
 output "emr_master_role_name" {
   value = var.create_emr_roles ? aws_iam_role.emr_master_role[0].name : null
