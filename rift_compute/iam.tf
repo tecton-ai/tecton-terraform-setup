95acc50c682382489d7e1cd9386390bdbcb205a6
Zhou Tan <zhou@tecton.ai>
2024-09-17 19:34:11 -0700

    runinstance permissions

--- rift_compute/iam.tf
+++ rift_compute/iam.tf
@@ -71,6 +71,7 @@ data "aws_iam_policy_document" "manage_rift_compute" {
     ]
     resources = flatten([
       "arn:aws:ec2:*::image/*", # TODO: Restrict to specific AMI ARN
+      "arn:aws:ec2:*:${local.account_id}:network-interface/*",
       aws_security_group.rift_compute.arn,
       [for subnet in aws_subnet.private : subnet.arn],
     ])
