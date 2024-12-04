5a9b1654454a928f966d96827ec1e36c5bc736b5
Emmanuel Shiferaw <emmanuel.shiferaw@tecton.ai>
2024-12-04 14:55:40 -0500

    Enable private dns for rift VPC when privatelink is enabled.

--- rift_compute/vpc.tf
+++ rift_compute/vpc.tf
@@ -27,7 +27,9 @@ module "public_private_subnet_cidrs" {
 }
 
 resource "aws_vpc" "rift" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_subnet" "private" {
